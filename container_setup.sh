1b22ac080fc2c338658320e802fd4ec8013d43d3
junlinnick <junlinnick@163.com>
2022-06-05 23:18:34 +0800

    add checkin project

--- container_setup.sh
+++ container_setup.sh
@@ -25,7 +25,7 @@ mv $setup_path/.repo  /etc/yum.repos.d
 yum clean all
 yum make cache
 #install anaconda
-bash ${setup_files}/Anaconda3-2021.05-Linux-x86_64.sh -b
+bash ${setup_files}/Anaconda3-2021.05-Linux-x86_64.sh-b
 echo "export PATH=/root/anaconda3/bin:$PATH" >> /root/.bashrc
 source ~/.bashrc
 
@@ -37,6 +37,10 @@ conda install -c conda-forge dlib  -y
 
 pip install cmake -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install face_recognition -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install django -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install pandas -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
 
 \cp  -f /etc/hosts.bak /etc/hosts
 yum clean all
---
+++ django_setup.sh
@@ -0,0 +1,12 @@
+tips:
+https://www.byhy.net/tut/webdev/django/04/
+https://www.bilibili.com/read/cv7444716/
+https://www.jianshu.com/p/b81abbf46e6c
+
+# 执行命令创建项目目录，并且进入到项目目录
+cd /data/checkin
+
+# 然后执行命令 创建manage.py 和 项目配置目录 名为 backend
+django-admin startproject backend 
+
+python manage.py migrate
