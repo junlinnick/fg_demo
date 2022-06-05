582dd0518883bfd46d485c0990efbcbb139f968a
junlinnick <junlinnick@163.com>
2022-06-05 17:21:34 +0800

    s

---
+++ container_setup.sh
@@ -0,0 +1,44 @@
+:<<CHECKIN
+
+
+CHECKIN
+
+print_log(){
+    ts=`data + "%F %X"`
+    echo "$ts [INFO]              $@"
+}
+
+# 检查root用户执行
+
+if  [ "$(id -u) "!= "0"]; then 
+        echo "This script must be run as root " 1 >&2
+        exit 1
+
+fi
+
+setup_path=/data/checkin/setup_files
+
+# nginx_conf=/etc/ngix/ngix.nginx_conf
+mkdir -p /etc/yum.repos.d.bak
+mv /etc/yum.repos.d/*  /etc/yum.repos.d.bak
+mv $setup_path/.repo  /etc/yum.repos.d
+yum clean all
+yum make cache
+#install anaconda
+bash ${setup_files}/Anaconda3-2021.05-Linux-x86_64.sh -b
+echo "export PATH=/root/anaconda3/bin:$PATH" >> /root/.bashrc
+source ~/.bashrc
+
+conda create -n env_dlib python=3.8 -y 
+
+conda activate env_dlib
+
+conda install -c conda-forge dlib  -y
+
+pip install cmake -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install face_recognition -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+\cp  -f /etc/hosts.bak /etc/hosts
+yum clean all
+rm -rf /var/cache/yum/*
+rm -rf  ~/.cache/pip
---
+++ docker_setup.sh
@@ -0,0 +1,37 @@
+today=`data   "+%Y%m%d"`
+author=jl
+commet="构建基础镜像${today}"
+host_setup_base_path=/data/checkin/base_images/
+image_id=8652b9f0cb4c
+version=v1
+
+img_name=checkin_sys
+image_tag=checkin_image_base_${today}_${version}
+container_name=checkin_container_${today}_${version}
+
+setup_files_path=$host_setup_base_path/setup_files_path
+host_mapped_path=/data/checkin/mapped_data
+container_base_path=/data/checkin
+
+container_setup_path=$container_base_path/setup_files
+container_mapped_path=$container_base_path/mapped
+
+docker  run \
+-v $host_mapped_path:$container_mapped_path  \
+--network bridge -d \
+--name $container_name \
+--privileged=true \
+$image_id /usr/sbin/init
+
+
+#copy files to container
+docker exec $container_name /bin/bash -c "mkdir -p  ${container_base_path"
+docker cp $host_setup_base_path $container_name:$container_base_path
+
+
+docker exec $container_name /bin/bash -c "sh $container_setup_path/container_setup.sh"
+docker exec $container_name /bin/bash -c  "rm -r  $container_setup_path"
+
+docker commit -m "$comment"  -a "author" $container_name $img_name:${image_tag}
+##test#
+ docker run -v /home/junlin/project/face_recognition_jl/checkin:/data/checkin -d --name checkin_sys_test1  --privileged=true 8652b9f0cb4c /usr/sbin/init
